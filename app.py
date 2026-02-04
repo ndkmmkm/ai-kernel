1494425b84c63eac8bf10b51e879975ffe5c6ff6
ndkmmkm <57156065+ndkmmkm@users.noreply.github.com>
2026-02-04 02:32:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -32,8 +32,8 @@ def ask():
 
 import os
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 3000))
-    app.run(host="0.0.0.0", port=port)
+port = int(os.environ.get("PORT", 3000))
+app.run(host="0.0.0.0", port=port)
+
 
 
