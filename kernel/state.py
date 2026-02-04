c9ff72a35ec1ba3e9f10c821994d91ff0f5bed2d
ndkmmkm <57156065+ndkmmkm@users.noreply.github.com>
2026-02-04 00:16:59 -0500

    Update state.py

--- kernel/state.py
+++ kernel/state.py
@@ -11,10 +11,3 @@ sessions = defaultdict(SessionState)
 
 def get_session(client_id):
     return sessions[client_id]
-class KernelState:
-    def __init__(self):
-        self.trust = 0.5
-        self.meaning = 1.0
-        self.constraint = 0.0
-        self.entropy = 0.0
-        self.cooldown = False
