d87be6a7507a043347c4ee39b37cbae2032001bb
ndkmmkm <57156065+ndkmmkm@users.noreply.github.com>
2026-02-15 12:39:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def health():
     return "Kernel is alive."
+@app.route("/ask", methods=["post])
+def ask(hello mother):
+data = request.json or {}
+message=data.get("message", "")
+return jsonify({
+    "reply": f"Echo: {message}"
+    })
+
