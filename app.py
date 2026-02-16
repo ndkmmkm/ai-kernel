0409c9b6d86b44f82639ee7f55f9674a530f227e
ndkmmkm <57156065+ndkmmkm@users.noreply.github.com>
2026-02-16 00:44:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,24 +9,12 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__)
 ai = load_core()
 
-@app.route("/", methods=["GET"])
-def home():
-    return jsonify({"status": "Kernel is running"})
-
-@app.route("/ask", methods=["POST"])
+@pp.route("/ask", methods=["post"])
 def ask():
-
-
     data = request.json or {}
-    message = data.get("message", "")
-
-    logger.info(f"Incoming message: {message}")
-
-    rreply = ai.respond(message)
-
-add_interaction(message, reply)
-
-logger.info(f"Outgoing reply: {reply}")
-
-
+    message =data.get(message", "")
+    logger.info(f"incoming message: {message}")
+    reply = ai.respond(message)
+    add_interaction(message, reply)
+    logger.info(f"Outgoing reply: {reply}")
     return jsonify({"reply": reply})
